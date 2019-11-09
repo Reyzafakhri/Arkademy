21090dd6a9529bf65509655657e76d17731fd142
Reyzafakhri <57551935+Reyzafakhri@users.noreply.github.com>
2019-11-09 19:36:00 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+def printwords(w):
+    for i in w:
+        if i in ['A', 'I', 'U', 'E', 'O']:
+            print(i)
+    for i in w:
+        if i in['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']:
+            print(i)
+printwords('ARKADEMY')
